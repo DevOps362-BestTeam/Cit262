78b2197eb8afb6850fd1b1895710a3450189a31d
Julio Maya <may21011@byui.edu>
2023-01-19 10:43:59 -0700

    goodbadtest

---
+++ tests/logiapi.test.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+
+it ("should get login token", async () => {//assync allows us to read from the database by moving on and come back
+
+    const loginResponse = await fetch('https://dev.stedi.me/login', { //await fetch allows it move on while getting the data 
+
+        method:'POST', //method : POST says to give information to the server
+        headers:{
+        'content-type':'application/text'
+        },
+
+        body:JSON.stringify({//makes the following into JSON text to be read by the server
+            "userName": "may21011@byui.edu",
+            "password": "P@ssw0rd"
+        })
+    })
+    expect(loginResponse.status).toBe(200); //means authorized
+    
+
+})
